2ba06435077861e7180bacf61a9e29961fb32993
Jonas Schievink <jonas.schievink@ferrous-systems.com>
2020-07-31 17:09:42 +0200

    expand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use ::tokio::macros::support::Future;
+use ::tokio::macros::support::Pin;
+use ::tokio::macros::support::Poll::{Pending, Ready};
 use std::sync::Mutex;
 
 #[allow(dead_code)]
@@ -10,14 +13,59 @@ enum Msg {
 fn main() {
     let (_, mut rx) = tokio::sync::mpsc::unbounded_channel::<Msg>();
     let entity = Mutex::new(());
-
     tokio::runtime::Builder::new()
         .build()
         .unwrap()
         .block_on(async move {
-            tokio::select! {
-                Some(_msg) = rx.recv() => {
-                    entity.lock();
+            {
+                mod util {
+                    pub(super) enum Out<_0> {
+                        _0(_0),
+                        Disabled,
+                    }
+                }
+                let output = {
+                    let mut futures = (rx.recv(),);
+                    ::tokio::future::poll_fn(|cx| {
+                        let mut is_pending = false;
+                        for _ in 0..1 {
+                            match 0 {
+                                0 => {
+                                    let (fut, ..) = &mut futures;
+                                    let fut = unsafe { Pin::new_unchecked(fut) };
+                                    let out = match fut.poll(cx) {
+                                        Ready(out) => out,
+                                        Pending => {
+                                            is_pending = true;
+                                            continue;
+                                        }
+                                    };
+                                    #[allow(unused_variables)]
+                                    match &out {
+                                        Some(_msg) => {}
+                                        _ => continue,
+                                    }
+                                    return Ready(util::Out::_0(out));
+                                }
+                                _ => unreachable!(
+                                    "reaching this means there probably is an off by one bug"
+                                ),
+                            }
+                        }
+                        if is_pending {
+                            Pending
+                        } else {
+                            Ready(util::Out::Disabled)
+                        }
+                    })
+                    .await
+                };
+                match output {
+                    util::Out::_0(Some(_msg)) => {
+                        entity.lock();
+                    }
+                    util::Out::Disabled => unreachable!(),
+                    _ => unreachable!("failed to match bind"),
                 }
             }
             entity.lock();
