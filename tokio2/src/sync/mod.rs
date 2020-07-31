b458bf2a19bb1e010b2c1d6cbf89ad8dba837609
Jonas Schievink <jonasschievink@gmail.com>
2020-07-31 22:50:47 +0200

    more of less

--- tokio2/src/sync/mod.rs
+++ tokio2/src/sync/mod.rs
@@ -1,13 +1,37 @@
-use std::task::Waker;
-
+use std::future::Future;
+use std::marker::PhantomData;
+use std::pin::Pin;
 use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
-
-use std::marker::PhantomData;
 use std::task::Poll::{Pending, Ready};
+use std::task::Waker;
 use std::task::{Context, Poll};
 
-use crate::future::poll_fn;
+/// Future for the [`poll_fn`] function.
+pub struct PollFn<F> {
+    f: F,
+}
+
+impl<F> Unpin for PollFn<F> {}
+
+/// Creates a new future wrapping around a function returning [`Poll`].
+pub fn poll_fn<T, F>(f: F) -> PollFn<F>
+where
+    F: FnMut(&mut Context<'_>) -> Poll<T>,
+{
+    PollFn { f }
+}
+
+impl<T, F> Future for PollFn<F>
+where
+    F: FnMut(&mut Context<'_>) -> Poll<T>,
+{
+    type Output = T;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<T> {
+        (&mut self.f)(cx)
+    }
+}
 
 struct AtomicWaker {}
 
@@ -20,32 +44,24 @@ impl AtomicWaker {
     fn register_by_ref(&self, _waker: &Waker) {}
 }
 
-/// Channel sender
 #[allow(dead_code)]
-pub(crate) struct Tx<T> {
+struct Tx<T> {
     inner: Arc<Chan<T>>,
 }
 
-/// Channel receiver
-pub(crate) struct Rx<T> {
+struct Rx<T> {
     inner: Arc<Chan<T>>,
 }
 
 #[allow(dead_code)]
 struct Chan<T> {
-    /// Handle to the push half of the lock-free list.
     tx: PhantomData<T>,
-
-    /// Coordinates access to channel's capacity.
     semaphore: Sema,
-
-    /// Receiver waker. Notified when a value is pushed into the channel.
     rx_waker: AtomicWaker,
-
     rx_closed: bool,
 }
 
-pub(crate) fn channel<T>() -> (Tx<T>, Rx<T>) {
+fn channel<T>() -> (Tx<T>, Rx<T>) {
     let chan = Arc::new(Chan {
         tx: PhantomData,
         semaphore: Sema(AtomicUsize::new(0)),
@@ -65,7 +81,7 @@ pub(crate) fn channel<T>() -> (Tx<T>, Rx<T>) {
 
 impl<T> Rx<T> {
     /// Receive the next value
-    pub(crate) fn recv(&mut self, cx: &mut Context<'_>) -> Poll<Option<T>> {
+    fn recv(&mut self, cx: &mut Context<'_>) -> Poll<Option<T>> {
         self.inner.rx_waker.register_by_ref(cx.waker());
 
         if self.inner.rx_closed && self.inner.semaphore.is_idle() {
@@ -76,8 +92,6 @@ impl<T> Rx<T> {
     }
 }
 
-// ===== impl Semaphore for AtomicUsize =====
-
 struct Sema(AtomicUsize);
 
 impl Sema {
@@ -86,25 +100,10 @@ impl Sema {
     }
 }
 
-/// Receive values from the associated `UnboundedSender`.
-///
-/// Instances are created by the
-/// [`unbounded_channel`](unbounded_channel) function.
 pub struct UnboundedReceiver<T> {
-    /// The channel receiver
     chan: Rx<T>,
 }
 
-/// Creates an unbounded mpsc channel for communicating between asynchronous
-/// tasks.
-///
-/// A `send` on this channel will always succeed as long as the receive half has
-/// not been closed. If the receiver falls behind, messages will be arbitrarily
-/// buffered.
-///
-/// **Note** that the amount of available system memory is an implicit bound to
-/// the channel. Using an `unbounded` channel has the ability of causing the
-/// process to run out of memory. In this case, the process will be aborted.
 pub fn unbounded_channel<T>() -> UnboundedReceiver<T> {
     let (tx, rx) = channel();
 
